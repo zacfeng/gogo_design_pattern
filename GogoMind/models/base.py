3e338712cf3e9b04314ac4d9c36ebb3240534638
Zac Chien <fengying0709@gmail.com>
2019-03-19 19:30:21 +0800

    complete

--- GogoMind/models/base.py
+++ GogoMind/models/base.py
@@ -1,5 +1,8 @@
 
 class MindMapModel:
+    def __init__(self, desc):
+        self.__map = None
+
     def create_mind_map(desc):
         pass
     
@@ -19,3 +22,29 @@ class SimpleNodeFactory:
         '''
         pass
 
+class Composite:
+    def get_map(self):
+        pass
+    def add_chid(self):
+        pass
+    def add_sibling(self):
+        pass
+    def get_nodes(self):
+        pass
+
+class Component:
+    def __init__(self, id, desc):
+        self.__id = id
+        self.__desc = desc
+
+    def add_child(self, node):
+        pass
+
+    def add_sibling(self, node):
+        pass
+
+    def get_nodes(self):
+        pass
+
+    def get_map(self):
+        pass
