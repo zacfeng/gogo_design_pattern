edb9e3b2bcdf092ecc54a4a4f7389e9ef74c3112
Zac Chien <fengying0709@gmail.com>
2019-03-19 18:57:22 +0800

    init

---
+++ GogoMind/models/base.py
@@ -0,0 +1,21 @@
+
+class MindMapModel:
+    def create_mind_map(desc):
+        pass
+    
+    def create_node(name):
+        pass
+
+    def insert_node(node, nid):
+        pass
+    
+    def save():
+        pass
+
+class SimpleNodeFactory:
+    def create_node():
+        '''
+            return Comnposite
+        '''
+        pass
+
